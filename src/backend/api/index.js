43ea332d8c5d33a33ac469e3452606f0a8116e9c
ryanmart25 <80070431+ryanmart25@users.noreply.github.com>
2024-02-20 20:11:28 -0800

    changed the location of html files.

--- src/backend/api/index.js
+++ src/backend/api/index.js
@@ -8,27 +8,8 @@ app.listen(
     PORT,
     () => console.log('I\'m alive at http://localhost:' + PORT)
 )
-//get
-/*
-app.get('/', (req, res) => {
-    res.status(200).send(<h1>hi</h1>)
-})
-app.get('/event', (req, res) => {
-    if (req.query.type == 'Career Fair') {
-        res.status(200).send({
-            event: 'Career Fair',
-            time: '12:00 PM'
-        })
-    }
-    else {
-        res.status(200).send({
-            event: 'No Event',
-            time: 'NA'
-        })
-    }
 
-});
-*/
+
 //patch
 
 //delete
