561fa3e67ec7dde18be9126eb060c31c55df55b9
John Campo <jjcampo@protonmail.com>
2024-04-16 16:41:43 -0700

    user is a model, not a service

--- src/main/java/com/group4/vms/User.java
+++ src/main/java/com/group4/vms/User.java
@@ -14,15 +14,4 @@ public class User {
     private boolean adminPrivs;
     private ObjectId[] events;  // use a stack/queue instead for fast insertion/deletion?
                                 // yes, TODO change from array to stack;
-
-    private String joinEvent(ObjectId eventId) { // Error, do not use until stack is implemented.
-        //if (/*is managing event*/); return "cannot join";
-        for (ObjectId objectId : events) {
-            if (objectId.equals(eventId)) {
-                return "event already exists";
-            }
-        }
-        //send join request to event manager?
-        return "request sent";
-    }
 }
