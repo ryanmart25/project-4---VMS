8fd789c8f1afdd7d0d2abfd5704f8f07608fdf9b
ryanmart25 <80070431+ryanmart25@users.noreply.github.com>
2024-04-17 00:15:38 -0700

    error is here

--- src/main/java/com/group4/vms/api/model/Employee.java
+++ src/main/java/com/group4/vms/api/model/Employee.java
@@ -4,21 +4,18 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "employees")
-public class Employee extends Person {
+public class Employee extends User{
 
-    private boolean adminPrivs;
+    private boolean adminPrivs = false;
 
-    public Employee(ObjectId id, String name, String pronouns) {
-        super(id, name, pronouns);
-        this.adminPrivs = true;
+    public Employee(ObjectId id, String name, String email, String password, String pronouns, boolean approved) {
+        super(id, name, email, password, pronouns, approved);
+
     }
 
-    public Employee() {
-        super();
-    }
 
     public void setAdminPrivs() {
         // should only be used to revoke admin privs from this Employee
-        this.adminPrivs = false;
+        this.adminPrivs = !this.adminPrivs;
     }
 }
--- src/main/java/com/group4/vms/api/model/User.java
+++ src/main/java/com/group4/vms/api/model/User.java
@@ -1,8 +1,24 @@
 package com.group4.vms.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "volunteers")
 public class User {
-
-    public User() {
-
-    }
+    @Id
+    ObjectId id;
+    String name;
+    String email;
+    String password;
+    String pronouns;
+    boolean approved;
 }
+
+
