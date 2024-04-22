eca7d564a4c3c94ef5e9ba515a43da4303174de9
John Campo <jjcampo@protonmail.com>
2024-04-22 14:41:11 -0700

    nothing crazy

--- src/main/java/com/group4/vms/api/repository/VolunteerRepository.java
+++ src/main/java/com/group4/vms/api/repository/VolunteerRepository.java
@@ -1,6 +1,9 @@
 package com.group4.vms.api.repository;
 
+import com.group4.vms.api.model.Employee;
 import com.group4.vms.api.model.User;
+import com.group4.vms.api.model.Volunteer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
