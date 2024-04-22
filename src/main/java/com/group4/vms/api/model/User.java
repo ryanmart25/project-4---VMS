ce3bd7d80ebf0ec02d468efbb9d3d8e7232f9c7d
John Campo <jjcampo@protonmail.com>
2024-04-22 14:28:34 -0700

    first bit

--- src/main/java/com/group4/vms/api/model/User.java
+++ src/main/java/com/group4/vms/api/model/User.java
@@ -1,19 +1,23 @@
-package com.group4.vms.model;
+package com.group4.vms.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "volunteers")
-public class Volunteer extends User{
-    
-    private boolean adminPrivs;
-    
-    public Volunteer(ObjectId id, String name, String email, String password, String pronouns) {
-        super(id, name, email, password, pronouns, false);
-    }
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@Document(collection = "volunteers")
+public class User {
+    @Id
+    ObjectId id;
+    String name;
+    String email;
+    String password;
+    String pronouns;
+    Boolean isEmployee;
 
-    public Volunteer() {
-        super();
-    }
 }
-
