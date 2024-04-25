9862d67d972fb52b9fd32133ef7d35db862aabe7
John Campo <jjcampo@protonmail.com>
2024-04-25 16:57:43 -0700

    event field

--- src/main/java/com/group4/vms/api/model/Event.java
+++ src/main/java/com/group4/vms/api/model/Event.java
@@ -3,15 +3,16 @@ package com.group4.vms.api.model;
 import java.util.Date;
 import java.util.LinkedList;
 
-import com.group4.vms.api.utility.TimeDuration;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import com.group4.vms.api.utility.TimeDuration;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 @Data
 @NoArgsConstructor
@@ -24,4 +25,5 @@ public class Event {
     TimeDuration duration;
     @DocumentReference
     private LinkedList<ObjectId> volunteerList;
+    private LinkedList<ObjectId> requestedVolunteers;
 }
